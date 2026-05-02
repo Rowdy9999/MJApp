03a4dc6f62c18a52bd64520797e6f608de9a93d1
Rowdy9999 <gajendra.kumar59@gmail.com>
2026-05-02 07:58:11 +0530

    Add Material Components library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.json:json:20231013")
+    implementation("com.google.android.material:material:1.11.0")
 }
